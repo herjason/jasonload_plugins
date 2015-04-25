d7a1ca56b52410904f79ecf5e8c5a9b69a0ac994
herjason <herjason@126.com>
2015-04-25 13:47:28 +0800

    优化单选插件

--- jasonload/leson/input_checkbox_radio.js
+++ jasonload/leson/input_checkbox_radio.js
@@ -31,16 +31,26 @@ define("leson/input_checkbox_radio",["jquery","./css/checkbox_radio.css"],functi
 		var self = this;
 		var opt=$.extend({},{clickCallback:function(){return true;}},options);
 		return $(':radio+label',self).each(function(){
-			$(this).addClass('leson_web_hRadio');
-			if($(this).prev().is(":checked"))
-				$(this).addClass('leson_web_hRadio_Checked');
+			$obj=$(this);
+			$obj.addClass('leson_web_hRadio');
+			if($obj.prev().is(":checked")&&!$obj.hasClass("leson_web_hRadio_Checked")){
+				$obj.addClass('leson_web_hRadio_Checked');
+			}
 		}).click(function(event){
+			var $obj=$(this);
 			if(opt.clickCallback(this)!==false){
-				$(this).siblings().removeClass("leson_web_hRadio_Checked");
-				if(!$(this).prev().is(':checked')){
-					$(this).addClass("leson_web_hRadio_Checked");
-					$(this).prev()[0].checked = true;
-				}
+				var $name = $obj.attr("group");
+				var $groups = $(self).find("label[group='" + $name +"']");
+				$groups.each(function(){
+					$(self).find("label[group='" + $name +"']").removeClass("leson_web_hRadio_Checked");
+					$obj.addClass("leson_web_hRadio_Checked");
+				})
+				
+				/*$obj.siblings().removeClass("leson_web_hRadio_Checked");
+				if(!$obj.prev().is(':checked')){
+					$obj.addClass("leson_web_hRadio_Checked");
+					$obj.prev()[0].checked = true;
+				}*/
 				event.stopPropagation();
 			}
 		}).prev().hide();
